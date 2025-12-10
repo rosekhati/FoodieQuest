2b84e5ba17dc6aad70f5ffb112a2b79cf2eb57e0
Fahri Firdausillah <elfaatta@gmail.com>
2025-12-10 10:43:24 +0700

    Remove sensitive Supabase credentials
    Removed hardcoded Supabase URL and Anon Key.

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -2,9 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
   // TODO: Replace with your actual Supabase URL and Anon Key
-  static const String supabaseUrl = 'https://znqlaaitfittxcujjuwp.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpucWxhYWl0Zml0dHhjdWpqdXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjE5MTUsImV4cCI6MjA3OTYzNzkxNX0.CzQ4Jzm2C3Nhbhj1LEYITgZaBAFEQZu2jZ4onnGnOcM';
+  static const String supabaseUrl = '';
+  static const String supabaseAnonKey = '';
 
   static final SupabaseClient client = Supabase.instance.client;
 
